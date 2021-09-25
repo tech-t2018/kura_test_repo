36a6a051712c4dc997062806e1e02a8361cbf135
tech-t2018 <86858802+tech-t2018@users.noreply.github.com>
2021-09-24 20:45:33 -0400

    Update first_test.js

--- cypress2/cypress/integration/first_test.js
+++ cypress2/cypress/integration/first_test.js
@@ -1,6 +1,6 @@
 describe('Testing input', () => {
     it('check input number', () => {
-        cy.visit('http://localhost:3000/example-2'),
+        cy.visit('http://172.31.31.38:3000/example-2'),
 
         cy.get('span')
         .invoke('text')
